bd5ad86ad258f7d53c9b9cfeb4be70a48086b17d
jona <uftak@student.kit.edu>
2024-07-22 16:23:05 +0200

    first test excluding h2

--- integrator.py
+++ integrator.py
@@ -33,16 +33,15 @@ class Integrator:
     def run(self):
         # create sym variables for y_i, T and t
         w_co2 = ca.SX.sym('w_co2', self.params.r_steps)
-        w_h2 = ca.SX.sym('w_h2', self.params.r_steps)
         w_ch4 = ca.SX.sym('w_ch4', self.params.r_steps)
         w_h2o = ca.SX.sym('w_h2o', self.params.r_steps)
+        w_h2 = 1 - w_co2 - w_ch4 - w_h2o
         T = ca.SX.sym('T', self.params.r_steps)
         t = ca.SX.sym('t')
         p = ca.SX.sym('p', self.params.r_steps)
 
         # create ode variables
         ode_co2 = ca.SX.sym('ode_co2', self.params.r_steps)
-        ode_h2 = ca.SX.sym('ode_h2', self.params.r_steps)
         ode_ch4 = ca.SX.sym('ode_ch4', self.params.r_steps)
         ode_h2o = ca.SX.sym('ode_h2o', self.params.r_steps)
         ode_T = ca.SX.sym('ode_T', self.params.r_steps)
@@ -50,16 +49,14 @@ class Integrator:
 
         # create surface variables
         w_co2_surf = ca.SX.sym('w_co2_surf')
-        w_h2_surf = ca.SX.sym('w_h2_surf')
         w_ch4_surf = ca.SX.sym('w_ch4_surf')
         w_h2o_surf = ca.SX.sym('w_h20_surf')
+        w_h2_surf = 1 - w_co2_surf - w_ch4_surf - w_h2o_surf
         T_surf = ca.SX.sym('T_surf')
 
         # create alg equations for the surface values
         alg_co2 = (self.params.w_co2_0
                    + self.params.delta_y * ca.sin(2 * ca.pi * self.params.f_y * t) - w_co2_surf)
-        alg_h2 = (self.params.w_h2_0
-                  - self.params.delta_y * ca.sin(2 * ca.pi * self.params.f_y * t) - w_h2_surf)
         alg_ch4 = (self.params.w_ch4_0
                    + 0 * self.params.delta_y * ca.sin(2 * ca.pi * self.params.f_y * t) - w_ch4_surf)
         alg_h2o = (self.params.w_h2o_0
@@ -68,8 +65,6 @@ class Integrator:
 
         D_co2_eff = ca.SX.sym('D_co2_eff', self.params.r_steps)
         alg_D_co2 = ca.SX.sym('alg_D_co2', self.params.r_steps)
-        D_h2_eff = ca.SX.sym('D_h2_eff', self.params.r_steps)
-        alg_D_h2 = ca.SX.sym('alg_D_h2', self.params.r_steps)
         D_ch4_eff = ca.SX.sym('D_ch4_eff', self.params.r_steps)
         alg_D_ch4 = ca.SX.sym('alg_D_ch4', self.params.r_steps)
         D_h2o_eff = ca.SX.sym('D_h2o_eff', self.params.r_steps)
@@ -85,8 +80,6 @@ class Integrator:
 
             ode_co2[i] = (self.diff.get_term(w_co2, w_co2_surf, i, D_co2_eff[i])
                           + self.reaction.get_mass_term(self.params.M_co2, roh_g, self.params.v_co2, r))
-            ode_h2[i] = (self.diff.get_term(w_h2, w_h2_surf, i, D_h2_eff[i])
-                         + self.reaction.get_mass_term(self.params.M_h2, roh_g, self.params.v_h2, r))
             ode_ch4[i] = (self.diff.get_term(w_ch4, w_ch4_surf, i, D_ch4_eff[i])
                           + self.reaction.get_mass_term(self.params.M_ch4, roh_g, self.params.v_ch4, r))
             ode_h2o[i] = (self.diff.get_term(w_h2o, w_h2o_surf, i, D_h2o_eff[i])
@@ -116,9 +109,6 @@ class Integrator:
             alg_D_co2[i] = ((self.params.epsilon / self.params.tau
                              * (1 / self.get_D_i_m(y_co2, y_h2, y_ch4, y_h2o, D_co2_h2, D_co2_ch4, D_co2_h2o)
                                 + 1e-6 / self.get_D_i_Kn(T[i], self.params.M_co2)) ** -1) - D_co2_eff[i])
-            alg_D_h2[i] = ((self.params.epsilon / self.params.tau
-                            * (1 / self.get_D_i_m(y_h2, y_co2, y_ch4, y_h2o, D_co2_h2, D_h2_ch4, D_h2_h2o)
-                               + 1e-6 / self.get_D_i_Kn(T[i], self.params.M_h2)) ** -1) - D_h2_eff[i])
             alg_D_ch4[i] = ((self.params.epsilon / self.params.tau
                              * (1 / self.get_D_i_m(y_ch4, y_h2, y_co2, y_h2o, D_h2_ch4, D_co2_ch4, D_ch4_h2o)
                                 + 1e-6 / self.get_D_i_Kn(T[i], self.params.M_ch4)) ** -1) - D_ch4_eff[i])
@@ -128,11 +118,11 @@ class Integrator:
 
         # create integrator
         dae = {
-            'x': ca.veccat(w_co2, w_h2, w_ch4, w_h2o, T),
-            'z': ca.vertcat(w_co2_surf, w_h2_surf, w_ch4_surf, w_h2o_surf, T_surf, D_co2_eff, D_h2_eff, D_ch4_eff, D_h2o_eff, p),
+            'x': ca.veccat(w_co2, w_ch4, w_h2o, T),
+            'z': ca.vertcat(w_co2_surf, w_ch4_surf, w_h2o_surf, T_surf, D_co2_eff, D_ch4_eff, D_h2o_eff, p),
             't': t,
-            'ode': ca.vertcat(ode_co2, ode_h2, ode_ch4, ode_h2o, ode_T),
-            'alg': ca.vertcat(alg_co2, alg_h2, alg_ch4, alg_h2o, alg_T, alg_D_co2, alg_D_h2, alg_D_ch4, alg_D_h2o, alg_p)
+            'ode': ca.vertcat(ode_co2, ode_ch4, ode_h2o, ode_T),
+            'alg': ca.vertcat(alg_co2, alg_ch4, alg_h2o, alg_T, alg_D_co2, alg_D_ch4, alg_D_h2o, alg_p)
         }
 
         options = {'regularity_check': True}
@@ -148,10 +138,12 @@ class Integrator:
         w_ch4_0 = np.full(self.params.r_steps, self.params.w_ch4_0)
         w_h2o_0 = np.full(self.params.r_steps, self.params.w_h2o_0)
         T_0 = np.full(self.params.r_steps, self.params.T_0)
-        x0 = ca.vertcat(w_co2_0, w_h2_0, w_ch4_0, w_h2o_0, T_0)
+        x0 = ca.vertcat(w_co2_0, w_ch4_0, w_h2o_0, T_0)
 
-        z0 = ca.vertcat(self.params.w_co2_0, self.params.w_h2_0, self.params.w_ch4_0, self.params.w_h2o_0,
-                        self.params.T_0, np.full(self.params.r_steps, 1), np.full(self.params.r_steps, 1), np.full(self.params.r_steps, 1), np.full(self.params.r_steps, 1), np.full(self.params.r_steps, 1))
+        z0 = ca.vertcat(self.params.w_co2_0, self.params.w_ch4_0, self.params.w_h2o_0,
+                        self.params.T_0, np.full(self.params.r_steps, 1),
+                        np.full(self.params.r_steps, 1), np.full(self.params.r_steps, 1),
+                        np.full(self.params.r_steps, 1))
 
         # integrate
         res = integrator(x0=x0, z0=z0)
@@ -160,10 +152,10 @@ class Integrator:
 
         # add surface values
         res_w_co2 = ca.vertcat(res_x[0], res_z[0])
-        res_w_h2 = ca.vertcat(res_x[1], res_z[1])
-        res_w_ch4 = ca.vertcat(res_x[2], res_z[2])
-        res_w_h2o = ca.vertcat(res_x[3], res_z[3])
-        res_T = ca.vertcat(res_x[4], res_z[4])
+        res_w_ch4 = ca.vertcat(res_x[1], res_z[1])
+        res_w_h2o = ca.vertcat(res_x[2], res_z[2])
+        res_w_h2 = 1 - res_w_co2 - res_w_ch4 - res_w_h2o
+        res_T = ca.vertcat(res_x[3], res_z[3])
         res_p = ca.vertcat(ca.vertcat(*res_z[-self.params.r_steps:]), res_z[-self.params.r_steps:][-1])
         res_err = 1 - res_w_co2.full() - res_w_h2.full() - res_w_ch4.full() - res_w_h2o.full()
 
@@ -172,9 +164,9 @@ class Integrator:
 
         # create plotter and plot
         plotter = Plotter(self.params.t_i, np.linspace(0, self.params.r_max, self.params.r_steps + 1),
-                          res_w_co2.full(), res_w_h2.full(), res_w_ch4.full(), res_w_h2o.full(), res_T.full(), res_p.full())
+                          res_w_co2.full(), res_w_h2.full(), res_w_ch4.full(), res_w_h2o.full(), res_T.full(),
+                          res_p.full())
 
         plotter.plot_w(20, 'Weight composition at t=0.2 s')
         plotter.plot_3d_all()
         plotter.plot_hm_all()
-
